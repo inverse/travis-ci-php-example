68db6181704032f4307968dbbe607d376cd07db4
Malachi Soord <inverse.chi@gmail.com>
2013-01-03 21:55:34 +0000

    Fixed issue with unit test

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -9,6 +9,4 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Hello Malachi!', $helloWorld->hello());
 		$this->assertEquals('Hello Bob!', $helloWorld->hello('Bob'));
     }
-
-
-
+}
